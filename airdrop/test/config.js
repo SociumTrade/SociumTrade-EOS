8829356da0166e31cf5678c1506526f4146fccfb
Pavel Kazimirenka <pavel@ittopgroup.com>
2018-10-18 14:13:22 +0300

    Implemented airdrop tests

---
+++ airdrop/test/config.js
@@ -0,0 +1,19 @@
+
+/** Configuration part */
+
+config = {
+    nodeURL : 'http://127.0.0.1:7777',
+    tokenSymbol : 'AAA',
+    tokenAccount : 'eosio.sm',
+    smartContractAccount : 'airdrop2',
+    testAccount : 'testnew',
+    defaultPrivateKey : "5KMK6m7jCqhxhEBnD1AVaPnLT17JfVdFPHGzLmSBC35LaHG5yNz", // smartContract user Private key
+    testKey1: "OS6VhPL9oDVw",
+    testKey2: "fGQZ4QSPciAR",
+    testKey3: "ImkuvmxzoRBQ",
+    testKey4: "0dQEn2RobNzN"
+}
+
+module.exports = config;
+
+/** Configuration part end */
